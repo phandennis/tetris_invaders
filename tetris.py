788404f56f7f1061e355e074966c1865cdaa5df8
Dennis <98193821+phandennis@users.noreply.github.com>
2022-11-25 14:09:35 -0800

    functional gameover screen

--- tetris.py
+++ tetris.py
@@ -1,25 +1,25 @@
 import pygame
-from screens import Welcome, GameScreen
-from base_game.screens import BaseScreen
+from screens import Welcome, GameScreen, GameOver
+from sprites import Player
 
 
-class Game:
+class Game(Player):
     """Main class for the application"""
 
     def __init__(self):
         # Creates the window
         self.window = pygame.display.set_mode((1280, 720))
 
+        pygame.display.set_caption("Tetris Shooter")
+
     def run(self):
         """Main method, manages interaction between screens"""
-
         # These are the available screens
         screens = {
             "welcome": Welcome,
-            "game": GameScreen
-            #"game_over": GameOverScreen,
+            "game": GameScreen,
+            "game_over": GameOver
         }
-
         # Start the loop
         running = True
         current_screen = "welcome"
@@ -28,13 +28,12 @@ class Game:
             screen_class = screens.get(current_screen)
             if not screen_class:
                 raise RuntimeError(f"Screen {current_screen} not found!")
-
+            
+            GameScreen.draw(self)
             # Create a new screen object, "connected" to the window
             screen = screen_class(self.window)
-
             # Run the screen
             screen.run()
-
             # When the `run` method stops, we should have a `next_screen` setup
             if screen.next_screen is False:
                 running = False
@@ -43,5 +42,5 @@ class Game:
 
 
 if __name__ == "__main__":
-    tetris = Game()
-    tetris.run()
+    tetris_shooter = Game()
+    tetris_shooter.run()

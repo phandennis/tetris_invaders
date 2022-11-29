d68c9d1d07d88ac9b9c08b0646d634f2ac6a5b8c
Dennis <98193821+phandennis@users.noreply.github.com>
2022-11-29 11:05:25 -0800

    added doc strings,  changed value for x axis change in movement and added ability to move up and down and can't go out of the screen on top

--- sprites/player.py
+++ sprites/player.py
@@ -8,6 +8,12 @@ from .mob import Mob
 class Player(pygame.sprite.Sprite):
 
     def __init__(self, player_image):
+        """Grabs the image and slaps a rect on the image
+        Allows you to blit the player
+
+        Args:
+            player_image (variable): it loads an image with pygame.image.load()
+        """
         pygame.sprite.Sprite.__init__(self)
         self.image = player_image
         self.rect = self.image.get_rect()
@@ -16,16 +22,23 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         """
-        Player's ability to move left or right and restricted from moving out of the screen
+        Player's ability to move left or right and 
+        restricted from moving out of the screen
         """
         self.movement = 0
+        self.movementy = 0
     # moves the player left or right
         keystroke = pygame.key.get_pressed()
         if keystroke[pygame.K_LEFT]:
-            self.movement += -1
+            self.movement += -5
         if keystroke[pygame.K_RIGHT]:
-            self.movement += +1
-        self.rect.x += self.movement
+            self.movement += +5
+        if keystroke[pygame.K_UP]:
+            self.movementy += -5
+        if keystroke[pygame.K_DOWN]:
+            self.movementy += +5
+
+        self.rect.centery += self.movementy
         self.rect.centerx += self.movement
 
     # makes sure player cannot get off screen
@@ -33,5 +46,9 @@ class Player(pygame.sprite.Sprite):
             self.rect.right = 1280
         if self.rect.left < 0:
             self.rect.left = 0
+        if self.rect.y > 720:
+            self.rect.y = 720
+        if self.rect.y < 0:
+            self.rect.y = 0
 
 

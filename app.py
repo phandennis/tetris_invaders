473ab5be450c8826831dbad27a520e85b389ec3b
Dennis <98193821+phandennis@users.noreply.github.com>
2022-11-29 11:00:49 -0800

    Flask that loads json file to display images with template directory that contains html

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import json
+from flask_api.grab_score import GrabScore
+import operator
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    data = 0
+    with open('./data/scores.json') as json_file:
+        data = json.load(json_file)
+        data.sort(key=operator.itemgetter("score"), reverse=True)
+    return render_template('list.html', data=data)
+
+@app.route("/sort_by_date")
+def sort_by_username():
+    data = 0
+    with open('./data/scores.json') as json_file:
+        data = json.load(json_file)
+        data.sort(key=operator.itemgetter("date"), reverse=True)
+    return render_template('list.html', data=data)
+
+if __name__ == '__main__':
+    app.run(host='localhost', debug=True)

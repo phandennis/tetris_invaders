f7e84316da4be56e3a83303d1dc4bac1993c64c8
Dennis <98193821+phandennis@users.noreply.github.com>
2022-11-29 11:04:04 -0800

    Added doc strings

--- sprites/projectile.py
+++ sprites/projectile.py
@@ -2,6 +2,13 @@ import pygame
 
 class Projectile(pygame.sprite.Sprite):
     def __init__(self, x , y, projectile_image):
+        """This is the projectile that the player shoots out in the space invader-like game
+
+        Args:
+            x (int): This sets the x position which was used to attach to the player position at the centerx 
+            y (int): It is used to attach to the player image on top
+            projectile_image (image): loads an image with pygame.image.load()
+        """
         pygame.sprite.Sprite.__init__(self)
         self.image = projectile_image
         self.rect = self.image.get_rect()
@@ -11,7 +18,7 @@ class Projectile(pygame.sprite.Sprite):
 
     def update(self):
         """
-         Sets the speed for the projectile and destroys the projectile if it is off-screen
+         Sets the speed for the projectile and destroys the projectile if it is off-screen (when it reachs the top)
         """
         self.rect.y += self.projectile_speed
 
